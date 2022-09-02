aad559e803c36b95b867af715628f636a76c9ebb
profedney <111137546+profedney@users.noreply.github.com>
2022-09-02 10:25:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcSum() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) + Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
+
+function calcSub() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) - Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
